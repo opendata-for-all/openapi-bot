7c61ffa5fbdbac2a98dd5b7cfba15bc98b706c6a
Gwendal <gwendal.daniel@gmail.com>
2019-07-31 15:02:48 +0200

    add GetLicense action and intent

---
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/action/GetLicense.java
@@ -0,0 +1,19 @@
+package com.xatkit.plugins.openapi.platform.action;
+
+import com.xatkit.core.platform.action.RuntimeAction;
+import com.xatkit.core.session.XatkitSession;
+import com.xatkit.plugins.openapi.platform.OpenAPIPlatform;
+import edu.uoc.som.openapi2.API;
+
+public class GetLicense extends RuntimeAction<OpenAPIPlatform> {
+
+    public GetLicense(OpenAPIPlatform platform, XatkitSession session) {
+        super(platform, session);
+    }
+
+    @Override
+    protected Object compute() throws Exception {
+        API api = (API) session.get(OpenAPIPlatform.LOADED_API_KEY);
+        return api.getInfo().getLicense().getName();
+    }
+}
