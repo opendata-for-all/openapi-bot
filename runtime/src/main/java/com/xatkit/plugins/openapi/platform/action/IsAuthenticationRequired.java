9698eefa4f2f7090da77e327ccd6ce81fd5ca4ea
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-07-31 16:24:53 +0200

    add action IsAuthenticationRequired

---
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/action/IsAuthenticationRequired.java
@@ -0,0 +1,21 @@
+package com.xatkit.plugins.openapi.platform.action;
+
+import com.xatkit.core.platform.action.RuntimeAction;
+import com.xatkit.core.session.XatkitSession;
+import com.xatkit.plugins.openapi.platform.OpenAPIPlatform;
+
+import edu.uoc.som.openapi2.API;
+
+public class IsAuthenticationRequired extends RuntimeAction<OpenAPIPlatform> {
+
+    public IsAuthenticationRequired(OpenAPIPlatform platform, XatkitSession session) {
+        super(platform, session);
+    }
+
+    @Override
+    protected Object compute() throws Exception {
+        API api = (API) session.get(OpenAPIPlatform.LOADED_API_KEY);
+        String result = api.getSecurity().isEmpty()?"No":"Yes";
+        return result;
+    }
+}
