d254d147fadb04e13704a47e639fba446f67620c
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-09-05 17:25:30 +0200

    Refactor the intent ListOperationForPath

---
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/action/ListOperationsOnPathUsingRowNumber.java
@@ -0,0 +1,56 @@
+package com.xatkit.plugins.openapi.platform.action;
+
+import com.xatkit.core.platform.action.RuntimeAction;
+import com.xatkit.core.session.XatkitSession;
+import com.xatkit.plugins.chat.platform.action.FormatList;
+import com.xatkit.plugins.openapi.platform.OpenAPIPlatform;
+import edu.uoc.som.openapi2.Operation;
+import edu.uoc.som.openapi2.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+public class ListOperationsOnPathUsingRowNumber extends RuntimeAction<OpenAPIPlatform> {
+
+    private int pathId;
+
+    public ListOperationsOnPathUsingRowNumber(OpenAPIPlatform platform, XatkitSession session, String pathIdentifier) {
+        super(platform, session);
+        pathId = Integer.parseInt(pathIdentifier);
+    }
+
+    @Override
+    protected Object compute() throws Exception {
+        List<Path> storedPaths = (List<Path>) session.get(FormatList.LAST_FORMATTED_LIST);
+        Path path = storedPaths.get(pathId);
+        List<Operation> operations = new ArrayList<>();
+        if(nonNull(path.getGet())) {
+            operations.add(path.getGet());
+        }
+        if(nonNull(path.getPost())) {
+            operations.add(path.getPost());
+        }
+        if(nonNull(path.getDelete())) {
+            operations.add(path.getDelete());
+        }
+        if(nonNull(path.getPatch())) {
+            operations.add(path.getPatch());
+        }
+        if(nonNull(path.getOptions())) {
+            operations.add(path.getOptions());
+        }
+        if(nonNull(path.getPut())) {
+            operations.add(path.getPut());
+        }
+        if(nonNull(path.getHead())) {
+            operations.add(path.getHead());
+        }
+        return operations;
+    }
+
+    private String createOperationDescription(Operation operation) {
+        return operation.getHTTPMethod() + ": " + operation.getDescription();
+    }
+}
