92b67ebd5bc6e2c9bbb994f2806116f912ec40eb
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-09-09 16:06:46 +0200

    Declare some formatters

--- runtime/src/main/java/com/xatkit/plugins/openapi/platform/OpenAPIPlatform.java
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/OpenAPIPlatform.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import org.apache.commons.configuration2.Configuration;
 
 import com.xatkit.core.XatkitCore;
+import com.xatkit.core.platform.Formatter;
 import com.xatkit.core.platform.RuntimePlatform;
 import com.xatkit.core.session.XatkitSession;
+import com.xatkit.plugins.openapi.platform.utils.FormatterUtils;
 
 import edu.uoc.som.openapi2.API;
 import edu.uoc.som.openapi2.Operation;
@@ -18,16 +20,21 @@ public class OpenAPIPlatform extends RuntimePlatform {
 
     public OpenAPIPlatform(XatkitCore xatkitCore, Configuration configuration) {
         super(xatkitCore, configuration);
-        this.xatkitCore.getFormatter("Default").registerFormatFunction(Path.class, Path::getRelativePath);
-        this.xatkitCore.getFormatter("Default").registerFormatFunction(Operation.class,
-                o -> o.getHTTPMethod() + ": " + o.getSummary());
-        this.xatkitCore.getFormatter("Default").registerFormatFunction(Map.Entry.class, e -> e.getKey()+"");
-//        Formatter formatter = new Formatter();
-//        this.xatkitCore.registerFormatter("LOL", formatter);
-//        formatter.registerFormatFunction(Path.class, p -> "lol" + p.getRelativePath());
+       registerFormatters();
     }
 
     public API getApi(XatkitSession session) {
         return (API) session.get(LOADED_API_KEY);
     }
+    
+    private void registerFormatters() {
+    	 this.xatkitCore.getFormatter("Default").registerFormatFunction(Path.class, Path::getRelativePath);
+         this.xatkitCore.getFormatter("Default").registerFormatFunction(Operation.class,
+                 o -> o.getHTTPMethod() + ": " + o.getSummary());
+         this.xatkitCore.getFormatter("Default").registerFormatFunction(Map.Entry.class, e -> e.getKey()+"");
+         Formatter formatter = new Formatter();
+         this.xatkitCore.registerFormatter("Minimal", formatter);
+         formatter.registerFormatFunction(Path.class,FormatterUtils::formatPath);
+    }
+   
 }
