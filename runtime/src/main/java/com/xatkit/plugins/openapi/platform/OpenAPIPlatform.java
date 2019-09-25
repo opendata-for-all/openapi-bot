bd93a41c4a88c881a77acb471b6301f80db4d4eb
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-09-25 18:33:31 +0200

    Add path context

--- runtime/src/main/java/com/xatkit/plugins/openapi/platform/OpenAPIPlatform.java
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/OpenAPIPlatform.java
@@ -3,6 +3,7 @@ package com.xatkit.plugins.openapi.platform;
 import java.util.Map;
 
 import org.apache.commons.configuration2.Configuration;
+import static java.util.Objects.nonNull;
 
 import com.xatkit.core.XatkitCore;
 import com.xatkit.core.platform.Formatter;
@@ -12,11 +13,13 @@ import com.xatkit.plugins.openapi.platform.utils.FormatterUtils;
 
 import edu.uoc.som.openapi2.API;
 import edu.uoc.som.openapi2.Operation;
+import edu.uoc.som.openapi2.Parameter;
 import edu.uoc.som.openapi2.Path;
 
 public class OpenAPIPlatform extends RuntimePlatform {
 
     public static final String LOADED_API_KEY = "xatkit.plugins.openapi.api";
+    public static final String PATH_IN_CONTEXT = "xatkit.plugins.openapi.path";
 
     public OpenAPIPlatform(XatkitCore xatkitCore, Configuration configuration) {
         super(xatkitCore, configuration);
@@ -30,8 +33,15 @@ public class OpenAPIPlatform extends RuntimePlatform {
     private void registerFormatters() {
     	 this.xatkitCore.getFormatter("Default").registerFormatFunction(Path.class, Path::getRelativePath);
          this.xatkitCore.getFormatter("Default").registerFormatFunction(Operation.class,
-                 o -> o.getHTTPMethod() + ": " + o.getSummary());
+                 o -> new StringBuilder().append(o.getHTTPMethod()).
+                 append(" ").
+                 append((nonNull(o.getOperationId()) && !o.getOperationId().equals(""))?"("+o.getOperationId()+")":"").
+                 append(": ").
+                 append (o.getSummary()).
+                 toString());
          this.xatkitCore.getFormatter("Default").registerFormatFunction(Map.Entry.class, e -> e.getKey()+"");
+         this.xatkitCore.getFormatter("Default").registerFormatFunction(Parameter.class,
+        		 p -> p.getName());
          Formatter formatter = new Formatter();
          this.xatkitCore.registerFormatter("Minimal", formatter);
          formatter.registerFormatFunction(Path.class,FormatterUtils::formatPath);
