4ff93565725d6fb895916e479b7a0f78eeb0fb13
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-10-03 12:16:26 +0200

    Get Operations returning schema

---
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/action/GetOperationsReturningSchema.java
@@ -0,0 +1,52 @@
+package com.xatkit.plugins.openapi.platform.action;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.xatkit.core.platform.action.RuntimeAction;
+import com.xatkit.core.session.XatkitSession;
+import com.xatkit.plugins.openapi.platform.OpenAPIPlatform;
+import com.xatkit.plugins.openapi.platform.utils.OpenAPIUtils;
+
+import edu.uoc.som.openapi2.API;
+import edu.uoc.som.openapi2.Operation;
+import edu.uoc.som.openapi2.Schema;
+
+public class GetOperationsReturningSchema extends RuntimeAction<OpenAPIPlatform>{
+	
+	private String schemaName;
+
+	public GetOperationsReturningSchema(OpenAPIPlatform runtimePlatform, XatkitSession session, String schemaName) {
+		super(runtimePlatform, session);
+		this.schemaName = schemaName;
+	}
+
+	@Override
+	protected Object compute() throws Exception {
+		 API api = runtimePlatform.getApi(session);
+		 Schema schema = api.getDefinitions().get(schemaName);
+		 Map<String, Object> result = new HashMap<String, Object>();
+		 
+		 if(schema == null) {
+				result.put("found", false);
+				result.put("reason", "SchemaNotFound");
+	    		result.put("options",api.getDefinitions().keySet().stream().collect(Collectors.toList()));
+		 }
+		 else {
+			 List<Operation> operations = OpenAPIUtils.getAllOperationsReturningSchema(api, schema);
+			 if(operations.isEmpty()) {
+			 result.put("found",false);
+			 result.put("reason", "Empty");}
+			 else  {
+				 result.put("found", true);
+				 result.put("value", operations);
+			 }
+		 }
+		return null;
+	}
+	
+	
+
+}
