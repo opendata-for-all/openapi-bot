fc3c411c917a25cfaf9ce7abc6bcbb276fdb0acf
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-10-03 12:10:01 +0200

    Refactor + add javadoc to the OpenAPIPlatform

--- runtime/src/main/java/com/xatkit/plugins/openapi/platform/OpenAPIPlatform.java
+++ runtime/src/main/java/com/xatkit/plugins/openapi/platform/OpenAPIPlatform.java
@@ -6,10 +6,8 @@ import org.apache.commons.configuration2.Configuration;
 import static java.util.Objects.nonNull;
 
 import com.xatkit.core.XatkitCore;
-import com.xatkit.core.platform.Formatter;
 import com.xatkit.core.platform.RuntimePlatform;
 import com.xatkit.core.session.XatkitSession;
-import com.xatkit.plugins.openapi.platform.utils.FormatterUtils;
 
 import edu.uoc.som.openapi2.API;
 import edu.uoc.som.openapi2.Operation;
@@ -17,40 +15,56 @@ import edu.uoc.som.openapi2.Parameter;
 import edu.uoc.som.openapi2.Path;
 import edu.uoc.som.openapi2.Property;
 
+/**
+ * A {@link RuntimePlatform} class that interacts with an OpenAPI definition.
+ *
+ */
 public class OpenAPIPlatform extends RuntimePlatform {
 
     public static final String LOADED_API_KEY = "xatkit.plugins.openapi.api";
     public static final String PATH_IN_CONTEXT = "xatkit.plugins.openapi.path";
 
+    
+    
+    /**
+     * Constructs a new {@link RuntimePlatform} from the provided {@link XatkitCore} and {@link Configuration}.
+     * <p>
+     * This constructor defines the formatters for the OpenAPI elements.
+     * 
+     * @param xatkitCore	the {@link XatkitCore} instance associated to this runtimePlatform
+     * @param configuration	the {@link Configuration} used to retrieve the OpenAPI properties
+     * @throws NullPointerException     if the provided {@code xatkitCore} or {@code configuration} is {@code null}
+     */
     public OpenAPIPlatform(XatkitCore xatkitCore, Configuration configuration) {
         super(xatkitCore, configuration);
-       registerFormatters();
+        this.xatkitCore.getFormatter("Default").registerFormatFunction(Path.class, Path::getRelativePath);
+        this.xatkitCore.getFormatter("Default").registerFormatFunction(Operation.class,
+                o -> new StringBuilder().append(o.getHTTPMethod()).
+                append(" ").
+                append((nonNull(o.getOperationId()) && !o.getOperationId().equals(""))?"("+o.getOperationId()+")":"").
+                append(": ").
+                append (o.getSummary()).
+                toString());
+        this.xatkitCore.getFormatter("Default").registerFormatFunction(Property.class, p -> new StringBuffer().append(p.getName())
+       		 .append(p.getRequired()?"(required)":"")
+       		 .toString());
+        
+        this.xatkitCore.getFormatter("Default").registerFormatFunction(Map.Entry.class, e -> e.getKey()+"");
+        this.xatkitCore.getFormatter("Default").registerFormatFunction(Property.class, p -> p.getName());
+        this.xatkitCore.getFormatter("Default").registerFormatFunction(Parameter.class,
+       		 p -> p.getName());
     }
 
+    
+   
+    /**
+     * Returns the loaded {@link API}
+     * 
+     * @param session	the {@link XatkitSession} instance 
+     * @return	the loaded {@link API}
+     */
     public API getApi(XatkitSession session) {
         return (API) session.get(LOADED_API_KEY);
     }
     
-    private void registerFormatters() {
-    	 this.xatkitCore.getFormatter("Default").registerFormatFunction(Path.class, Path::getRelativePath);
-         this.xatkitCore.getFormatter("Default").registerFormatFunction(Operation.class,
-                 o -> new StringBuilder().append(o.getHTTPMethod()).
-                 append(" ").
-                 append((nonNull(o.getOperationId()) && !o.getOperationId().equals(""))?"("+o.getOperationId()+")":"").
-                 append(": ").
-                 append (o.getSummary()).
-                 toString());
-         this.xatkitCore.getFormatter("Default").registerFormatFunction(Property.class, p -> new StringBuffer().append(p.getName())
-        		 .append(p.getRequired()?"(required)":"")
-        		 .toString());
-         
-         this.xatkitCore.getFormatter("Default").registerFormatFunction(Map.Entry.class, e -> e.getKey()+"");
-         this.xatkitCore.getFormatter("Default").registerFormatFunction(Property.class, p -> p.getName());
-         this.xatkitCore.getFormatter("Default").registerFormatFunction(Parameter.class,
-        		 p -> p.getName());
-         Formatter formatter = new Formatter();
-         this.xatkitCore.registerFormatter("Minimal", formatter);
-         formatter.registerFormatFunction(Path.class,FormatterUtils::formatPath);
-    }
-   
 }
